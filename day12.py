2634dce90f80f227e00e1b1f2b10f30b4c39d1ac
SnehaAS-12 <85794682+SnehaAS-12@users.noreply.github.com>
2021-06-24 12:45:33 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,14 @@
+#Create a file 30 days 30 hour operations
+#Write data in it I have completed 10 days successfully.
+#Append the data your name in to it.
+#Close the file.
+
+file=open("30 days 30 hour operations.txt","w+")
+file.write("I have completed 10 days successfully")
+file.close()
+
+file1 =open("30 days 30 hour operations.txt","a+") 
+file1.writelines("\nSneha")
+file1 =open("30 days 30 hour operations.txt","r") 
+print(file1.read())
+file1.close()
